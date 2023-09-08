d13977be12292af67160d7048c98bb4dcd7a1810
akbarsenawjy26 <wijayasenaakbar@gmail.com>
2023-09-08 14:37:52 +0700

    commit pertama folder program yang berisi reciver_popsi dan transmiter_popsi

---
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -0,0 +1,78 @@
+#include <Arduino.h>
+
+#ifdef ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+int pushUpSkor = 0, pushUpThresholdVL = 15;
+bool flag = false;
+
+VL53L0X sensor;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  Wire.begin();
+  sensor.init();
+  sensor.setTimeout(500);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3C for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+
+  if (!sensor.init())
+  {
+    Serial.println("Failed to initialize VL53L0X sensor!");
+    while (1)
+      ;
+  }
+
+  sensor.startContinuous();
+}
+
+void loop()
+{
+  uint16_t VLdistance = sensor.readRangeContinuousMillimeters() / 10;
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("VL-mode");
+  display.setCursor(0, 30);
+  display.println("Count:");
+  display.setCursor(80, 30);
+  display.println(pushUpSkor);
+  display.display();
+
+  if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
+  {
+    pushUpSkor += 1;
+    Serial.print("Skor Push Up = ");
+    Serial.println(pushUpSkor);
+    flag = true;
+  }
+  if (VLdistance > pushUpThresholdVL)
+  {
+    flag = false;
+  }
+
+  delay(100);
+}
