07f6eafdebb41eddd939ec2d4738bc6e5b22f363
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2023-09-12 06:40:59 +0700

    Update main.cpp
    manambah milis

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -7,11 +7,13 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-int pushUpSkor = 0, pushUpThresholdVL = 15;
+int pushUpSkor = 0, pushUpThresholddown = 15, pushUpThresholdup = 15;
 bool flag = false;
 
 VL53L0X sensor;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+unsigned long waktu_skrg = 0, waktu_sblm = 0, interval = 500;
 
 void setup()
 {
@@ -24,7 +26,7 @@ void setup()
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   { // Address 0x3C for 128x64
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
+    while(1)
       ; // Don't proceed, loop forever
   }
   display.clearDisplay();
@@ -32,7 +34,6 @@ void setup()
   if (!sensor.init())
   {
     Serial.println("Failed to initialize VL53L0X sensor!");
-    Serial2.println("Failed to initialize VL53L0X sensor!");
     while (1)
       ;
   }
@@ -44,29 +45,33 @@ void loop()
 {
   uint16_t VLdistance = sensor.readRangeContinuousMillimeters() / 10;
 
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("VL-mode");
-  display.setCursor(0, 30);
-  display.println("Count:");
-  display.setCursor(80, 30);
-  display.println(pushUpSkor);
-  Serial2.println(pushUpSkor);
-  display.display();
+  waktu_skrg = millis();
+  if(waktu_skrg-waktu_sblm >= interval){
 
-  if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("VL-mode");
+    display.setCursor(0, 30);
+    display.println("Count:");
+    display.setCursor(80, 30);
+    display.println(pushUpSkor);
+    display.display();
+
+    waktu_sblm = waktu_skrg;
+  }
+
+  if (VLdistance <= pushUpThresholddown && flag == false && VLdistance != 0)
   {
     pushUpSkor += 1;
-    Serial.print("Skor Push Up = ");
     Serial.println(pushUpSkor);
+    Serial2.println(pushUpSkor);
     flag = true;
   }
-  if (VLdistance > pushUpThresholdVL)
+  if (VLdistance > pushUpThresholdup)
   {
     flag = false;
   }
 
-  delay(100);
 }
