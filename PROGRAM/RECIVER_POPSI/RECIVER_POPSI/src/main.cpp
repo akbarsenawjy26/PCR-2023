7fd6d7714b42b3c85a0650988139c2c6c84a2445
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2023-10-20 05:46:10 +0700

    Update
    Base program serial

--- PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
+++ PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
@@ -1,20 +1,75 @@
 #include <Arduino.h>
 
-char myData[50];
+int Value1 = 0;
+int Value2 = 0;
+int Value3 = 0;
 
-void setup()
-{
-  Serial.begin(57600);
-  Serial2.begin(9600);
+
+int jumlah_data = 0;
+int jumlah_data_serial=0;
+
+const int panjang_data = 1+6+2;
+int i = 0;
+byte nilai[panjang_data];
+
+unsigned long detik_skrg = 0;
+unsigned long detik_sblm = 0;
+const long jarak_waktu = 200;
+
+
+void checksum_serial(){
+  byte kode = 0x42;
+  jumlah_data = 0;
+  jumlah_data_serial = 0;
+  
+  if(nilai[0] == kode){
+    for(int j = 1;j<panjang_data-2;j++){
+      jumlah_data += nilai[j];
+    }
+    jumlah_data_serial = nilai[panjang_data-2] << 8 | nilai[panjang_data-1];
+
+    if(jumlah_data == jumlah_data_serial){
+      Value1 = (nilai[1] << 8) | nilai[2];
+      Value2 = (nilai[3] << 8) | nilai[4];
+      Value3 = (nilai[5] << 8) | nilai[6];
+    }else{
+      Serial.print("GAGAL!");
+    }
+    
+  }else{
+    Serial.print("GAGAL!");
+  }
 }
 
-void loop()
-{
-  if (Serial2.available() > 0)
-  {
-    byte m = Serial2.readBytesUntil('\n', myData, 50);
-    myData[m] = '\0';
-    Serial.println(myData);
+int incomingByte = 0; // for incoming serial data
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(9600);
+
+}
+
+void loop() { 
+  if (Serial2.available() > 0) {
+    nilai[i] = Serial2.read();
+    i++;
+    if(i > panjang_data-1){
+      i = 0;
+    }
   }
-  delay(1000);
+  for(int k = 0; k<panjang_data;k++){
+    Serial.print(nilai[k]);
+  }
+  Serial.println();
+  
+  checksum_serial();
+
+  detik_skrg = millis();
+  if(detik_skrg-detik_sblm >= jarak_waktu){
+    detik_sblm = detik_skrg;
+    Serial.print(Value1);Serial.print("\t");
+    Serial.print(Value2);Serial.print("\t");
+    Serial.print(Value3);Serial.println();
+  }
+
 }
--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -1,72 +1,51 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <VL53L0X.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+//Uji Coba Checksum
+int Value1 = 150;
+int Value2 = 100;
+int Value3 = 89;
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+long int detik_skrg = 0;
+long int detik_sblm = 0;
+long int jarak_waktu = 100;
 
-int pushUpSkor = 0, pushUpThresholdVL = 15;
-bool flag = false;
+void checksum_serial(){
+  int panjang_data = 6;
+  byte nilai[panjang_data];
+  byte kode = 0x42;
+  byte checksum[2];
+  int jumlah_data = 0;
+  
+  nilai[0] = (Value1 >> 8) & 0xFF;
+  nilai[1] = Value1 & 0xFF;
+  nilai[2] = (Value2 >> 8) & 0xFF;
+  nilai[3] = Value2 & 0xFF;
+  nilai[4] = (Value3 >> 8) & 0xFF;
+  nilai[5] = Value3 & 0xFF;
 
-VL53L0X sensor;
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-void setup()
-{
-  Serial.begin(57600);
-  Serial2.begin(9600);
-  Wire.begin();
-  sensor.init();
-  sensor.setTimeout(500);
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  { // Address 0x3C for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ; // Don't proceed, loop forever
-  }
-  display.clearDisplay();
-
-  if (!sensor.init())
-  {
-    Serial.println("Failed to initialize VL53L0X sensor!");
-    Serial2.println("Failed to initialize VL53L0X sensor!");
-    while (1)
-      ;
+  for(int i=0;i < panjang_data; i++){
+    jumlah_data += nilai[i];
   }
 
-  sensor.startContinuous();
+  checksum[0] = (jumlah_data >> 8) & 0xFF;
+  checksum[1] = jumlah_data & 0xFF;
+
+  Serial2.write(kode);
+  Serial2.write(nilai,panjang_data);
+  Serial2.write(checksum,2);
 }
 
-void loop()
-{
-  uint16_t VLdistance = sensor.readRangeContinuousMillimeters() / 10;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial2.begin(9600);
 
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("VL-mode");
-  display.setCursor(0, 30);
-  display.println("Count:");
-  display.setCursor(80, 30);
-  display.println(pushUpSkor);
-  Serial2.println(pushUpSkor);
-  display.display();
+}
 
-  if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
-  {
-    pushUpSkor += 1;
-    Serial.print("Skor Push Up = ");
-    Serial.println(pushUpSkor);
-    flag = true;
+void loop() {
+  detik_skrg = millis();
+  if(detik_skrg-detik_sblm >= jarak_waktu){
+    detik_sblm = detik_skrg;
+    checksum_serial();
   }
-  if (VLdistance > pushUpThresholdVL)
-  {
-    flag = false;
-  }
-
-  delay(100);
 }
