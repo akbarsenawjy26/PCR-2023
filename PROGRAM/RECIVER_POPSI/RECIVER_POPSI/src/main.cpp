1bef9f3710f7610376ce310a32907d724e7fbdf1
korosen123 <111441726+korosen123@users.noreply.github.com>
2023-10-22 06:06:50 +0700

    address dan 4 digit value
    mengirimkan 1 valu angka 4 digit dan menambahkan address 0x45 yang dikirimkan setelah byte kode 0x42. address 0x45 diindikasikan sebagai device 1

--- PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
+++ PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
 int Value1 = 0;
-int Value2 = 0;
-int Value3 = 0;
+// int Value2 = 0;
+// int Value3 = 0;
 
 
 int jumlah_data = 0;
 int jumlah_data_serial=0;
 
-const int panjang_data = 1+6+2;
+const int panjang_data = 1+1+2+2;
 int i = 0;
 byte nilai[panjang_data];
 
@@ -16,22 +16,31 @@ unsigned long detik_skrg = 0;
 unsigned long detik_sblm = 0;
 const long jarak_waktu = 200;
 
-
 void checksum_serial(){
   byte kode = 0x42;
   jumlah_data = 0;
   jumlah_data_serial = 0;
   
   if(nilai[0] == kode){
-    for(int j = 1;j<panjang_data-2;j++){
+    switch (nilai[1])
+    {
+    case 0x45:
+      Serial.print("From Device 1"); Serial.println();
+      break;
+    
+    default:
+      break;
+    }
+
+    for(int j = 2;j<panjang_data-2;j++){
       jumlah_data += nilai[j];
     }
     jumlah_data_serial = nilai[panjang_data-2] << 8 | nilai[panjang_data-1];
 
     if(jumlah_data == jumlah_data_serial){
-      Value1 = (nilai[1] << 8) | nilai[2];
-      Value2 = (nilai[3] << 8) | nilai[4];
-      Value3 = (nilai[5] << 8) | nilai[6];
+      Value1 = (nilai[2] << 8) | nilai[3];
+      // Value2 = (nilai[3] << 8) | nilai[4];
+      // Value3 = (nilai[5] << 8) | nilai[6];
     }else{
       Serial.print("GAGAL!");
     }
@@ -67,9 +76,11 @@ void loop() {
   detik_skrg = millis();
   if(detik_skrg-detik_sblm >= jarak_waktu){
     detik_sblm = detik_skrg;
-    Serial.print(Value1);Serial.print("\t");
-    Serial.print(Value2);Serial.print("\t");
-    Serial.print(Value3);Serial.println();
+    Serial.print(Value1);
+    // Serial.print("\t");
+    // Serial.print(Value2);Serial.print("\t");
+    // Serial.print(Value3);
+    Serial.println();
   }
 
 }
--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -1,26 +1,27 @@
 #include <Arduino.h>
 //Uji Coba Checksum
-int Value1 = 150;
-int Value2 = 100;
-int Value3 = 89;
+int Value1 = 9999;
+// int Value2 = 100;
+// int Value3 = 89;
 
 long int detik_skrg = 0;
 long int detik_sblm = 0;
 long int jarak_waktu = 100;
 
 void checksum_serial(){
-  int panjang_data = 6;
+  int panjang_data = 2;
   byte nilai[panjang_data];
   byte kode = 0x42;
+  byte address = 0x45;
   byte checksum[2];
   int jumlah_data = 0;
   
   nilai[0] = (Value1 >> 8) & 0xFF;
   nilai[1] = Value1 & 0xFF;
-  nilai[2] = (Value2 >> 8) & 0xFF;
-  nilai[3] = Value2 & 0xFF;
-  nilai[4] = (Value3 >> 8) & 0xFF;
-  nilai[5] = Value3 & 0xFF;
+  // nilai[2] = (Value2 >> 8) & 0xFF;
+  // nilai[3] = Value2 & 0xFF;
+  // nilai[4] = (Value3 >> 8) & 0xFF;
+  // nilai[5] = Value3 & 0xFF;
 
 
   for(int i=0;i < panjang_data; i++){
@@ -31,8 +32,19 @@ void checksum_serial(){
   checksum[1] = jumlah_data & 0xFF;
 
   Serial2.write(kode);
+  Serial2.write(address);
   Serial2.write(nilai,panjang_data);
   Serial2.write(checksum,2);
+
+  Serial.print(kode);
+  Serial.print(address);
+  for(int i = 0; i < panjang_data; i++) {
+    Serial.print(nilai[i]);
+  }
+  for(int i = 0; i < 2; i++) {
+    Serial.print(checksum[i]);
+  }
+  Serial.println();
 }
 
 void setup() {
