0ea65fdee762b326353e82149269ed077abc6328
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2023-10-31 23:33:30 +0700

    update
    hanya update angka

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -20,7 +20,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-unsigned long waktu_skrg = 0, waktu_sblm = 0, interval = 500;
+unsigned long waktu_skrg = 0, waktu_sblm = 0, interval = 100;
 
 void setup()
 {
