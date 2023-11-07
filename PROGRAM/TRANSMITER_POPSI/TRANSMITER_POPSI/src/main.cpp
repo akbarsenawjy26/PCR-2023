9abfa938d50f50f0fce685d14c2e40ddf7e3296c
Ikmalmughni <ikmalmughni@gmail.com>
2023-11-07 20:35:11 +0700

    FIX send data
    yang sudah dibetulkan dan dapat mengirim data random

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -10,52 +10,96 @@ int mqttPort = 1883; // Port broker MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-int sensorValue = 56; // Nilai integer
+int adressDevice = 57; // Nilai integer
 const char* topicPrefix = "/esp32/data/sensor"; // String topik awal
+unsigned long lastMsg = 0;
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  // Callback jika Anda ingin menangani pesan yang diterima dari broker MQTT
-  // Anda dapat menambahkan logika khusus di sini.
-}
+void setup_wifi() {
 
-void setup() {
-  Serial.begin(115200);
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("Connected to WiFi");
+  randomSeed(micros());
 
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
   while (!client.connected()) {
-    if (client.connect("ESP32Client")) {
-      Serial.println("Connected to MQTT broker");
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.subscribe("/esp32/mqtt/in");
     } else {
-      Serial.print("Failed with state ");
+      Serial.print("failed, rc=");
       Serial.print(client.state());
-      delay(2000);
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
   }
 }
 
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
+}
+
+
+
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  long now = millis();
+  if (now - lastMsg > 1000) {
+    lastMsg = now;
+  
   // Gabungkan nilai integer dan string
   char fullTopic[50]; // Array char untuk menyimpan topik MQTT lengkap
-  snprintf(fullTopic, sizeof(fullTopic), "%s/%d", topicPrefix, sensorValue);
-
+  snprintf(fullTopic, sizeof(fullTopic), "%s/%d", topicPrefix, adressDevice);
   int randomData = random(0, 101);
-
   client.publish(fullTopic, String(randomData).c_str());
 
   //client.publish(fullTopic, randomData);
-  Serial.println("Data terkirim ke topik: " + String(fullTopic));
+  Serial.println("Data terkirim ke topik: " + String(randomData));
   client.loop();
   delay(1000);
+  }
   /* 
   // Kirim data dengan topik lengkap
   if (client.publish(fullTopic, "horee")) {
