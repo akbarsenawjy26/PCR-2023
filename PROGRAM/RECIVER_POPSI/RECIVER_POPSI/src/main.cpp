aad6ef05c541535a74245a768ad9138b6543511d
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2023-11-09 05:30:59 +0700

    update library
    transmiter dan receiver beda library

--- PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
+++ PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define OLED_SDA 12
+#define OLED_SCL 14
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_SH1106 display(12, 14);
+
+
+void setup()   {                
+  Serial.begin(115200);
+  /* initialize OLED with I2C address 0x3C */
+  display.begin(SH1106_SWITCHCAPVCC, 0x3C); 
+  display.clearDisplay();
+
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() { 
+  /* set text size, color, cursor position, 
+  set buffer with  Hello world and show off*/
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
 }
