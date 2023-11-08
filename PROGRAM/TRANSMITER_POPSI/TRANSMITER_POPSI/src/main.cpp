3a4b8d1b10abbb39b7686563a592ab1454e61127
Ikmalmughni <ikmalmughni@gmail.com>
2023-11-08 21:36:01 +0700

    gabungan mqtt send data dan main
    saya menggabungkan main dan mqtt send data pak, sebentar lagi saya ujicoba, mungkin kalau pak sayyid ingin mengecek dulu pak

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -1,12 +1,30 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 #include <Wire.h>
 #include <VL53L0X.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//-----------------------------DEFINE MQTT-------------------------------------------------------------
+
+const char* ssid = "TP-Link_AFBC"; // Nama jaringan WiFi
+const char* password = "Penelitian2023"; // Kata sandi WiFi
+const char* mqttServer = "broker.mqtt-dashboard.com"; // Alamat broker MQTT
+int mqttPort = 1883; // Port broker MQTT
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int adressDevice = 57; // Nilai integer
+const char* topicPrefix = "/esp32/data/sensor"; // String topik awal
+unsigned long lastMsg = 0;
+
+//-----------------------------DEFINE MQTT-------------------------------------------------------------
 
 int pushUpSkor = 0, pushUpThresholdVL = 15;
 bool flag = false;
@@ -19,10 +37,72 @@ int x = 0;
 double rata;
 bool start = false;
 
+//------------------------------PROGRAM MQTT----------------------------------------------------------
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.subscribe("/esp32/mqtt/in");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//------------------------------PROGRAM MQTT----------------------------------------------------------
+
 void setup()
 {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+  setup_wifi();
+  //WiFi.mode(WIFI_STA);
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
 
   Wire.begin();
   sensor.init();
@@ -46,8 +126,24 @@ void setup()
   sensor.startContinuous();
 }
 
+//-------------------------------------RUN----------------------------------------
+
+
+//-------------------------------------RUN----------------------------------------
+
+
 void loop()
 {
+ if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  long now = millis();
+  if (now - lastMsg > 1000) {
+    lastMsg = now;
+
+
+
   if (start)
   {
     uint16_t VLdistance = sensor.readRangeContinuousMillimeters();
@@ -57,7 +153,6 @@ void loop()
     if (i == 50)
     {
       rata = x / i;
-      // Serial.println("Rata2 = " + (String)rata);
       i = 0;
       x = 0;
       start = false;
@@ -70,30 +165,7 @@ void loop()
       start = true;
     }
   }
-
-
-  // display.clearDisplay();
-  // display.setTextSize(2);
-  // display.setTextColor(WHITE);
-  // display.setCursor(0, 0);
-  // display.println("VL-mode");
-  // display.setCursor(0, 30);
-  // display.println("Count:");
-  // display.setCursor(80, 30);
-  // display.println(pushUpSkor);
-  // display.display();
-
-  // if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
-  // {
-  //   pushUpSkor += 1;
-  //   Serial.print("Skor Push Up = ");
-  //   Serial.println(pushUpSkor);
-  //   flag = true;
-  // }
-  // if (VLdistance > pushUpThresholdVL)
-  // {
-  //   flag = false;
-  // }
-
-  delay(100);
+  client.loop();
+  delay(1000);   // Kirim data setiap 1 detik*/
+  }
 }
