4c5a6523ad929bc6422075869948f111aa03437a
Ikmalmughni <ikmalmughni@gmail.com>
2023-11-09 23:26:42 +0700

    ubidots mengirim menerima
    prototipe ubidots, bukan dengan program main

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -1,26 +1,56 @@
 /****************************************
  * Include Libraries
  ****************************************/
-#include "UbidotsESPMQTT.h"
+//#include "UbidotsESPMQTT.h"
 
 /****************************************
  * Define Constants
  ****************************************/
+ /*
 #define TOKEN "BBUS-R7nOjqo6WS530rYHaAGmGsrjSr8duh" // Your Ubidots TOKEN
 #define WIFINAME "TP-Link_AFBC" //Your SSID
 #define WIFIPASS "Penelitian2023" // Your Wifi Pass
+*/
+/******************************************
+ *
+ * This example works for both Industrial and STEM users.
+ *
+ * Developed by Jose Garcia, https://github.com/jotathebest/
+ *
+ * ****************************************/
 
-Ubidots client(TOKEN);
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsEsp32Mqtt.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+const char *UBIDOTS_TOKEN = "BBUS-R7nOjqo6WS530rYHaAGmGsrjSr8duh";  // Put here your Ubidots TOKEN
+const char *WIFI_SSID = "Kurniasyah";      // Put here your Wi-Fi SSID
+const char *WIFI_PASS = "Izzulizzam2002";      // Put here your Wi-Fi password
+const char *DEVICE_LABEL = "ESP32DevKit";   // Put here your Device label to which data  will be published
+const char *VARIABLE_LABEL = "Device-1"; // Put here your Variable label to which data  will be published
+
+const int PUBLISH_FREQUENCY = 500; // Update rate in milliseconds
+
+unsigned long timer;
+uint8_t analogPin = 32; // Pin used to read data from GPIO34 ADC_CH6.
+
+Ubidots ubidots(UBIDOTS_TOKEN);
 
 /****************************************
  * Auxiliar Functions
  ****************************************/
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i=0;i<length;i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
@@ -30,24 +60,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
  * Main Functions
  ****************************************/
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  //client.ubidotsSetBroker("industrial.api.ubidots.com"); // Sets the broker properly for the industrial account
-  client.setDebug(true); // Pass a true or false bool value to activate debug messages
   Serial.begin(115200);
-  client.wifiConnection(WIFINAME, WIFIPASS);
-  client.begin(callback);
-  }
+  // ubidots.setDebug(true);  // uncomment this to make debug messages available
+  ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
+  ubidots.setCallback(callback);
+  ubidots.setup();
+  ubidots.reconnect();
 
-void loop() {
+  timer = millis();
+}
+
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if(!client.connected()){
-      client.reconnect();
-      }
-  
-  float value1 = analogRead(A0);
-  client.add("temperature", value1);
-  client.ubidotsPublish("version1");
-  client.loop();
-  delay(500);
+  if (!ubidots.connected())
+  {
+    ubidots.reconnect();
+  }
+  if ((millis() - timer) > PUBLISH_FREQUENCY) // triggers the routine every 5 seconds
+  {
+    float value = analogRead(analogPin);
+    ubidots.add(VARIABLE_LABEL, value); // Insert your variable Labels and the value to be sent
+    ubidots.publish(DEVICE_LABEL);
+    timer = millis();
+  }
+  ubidots.loop();
 }
