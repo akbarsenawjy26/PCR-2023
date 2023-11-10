ad2ea9ce54c81cb8f0ebad2bc575ead2dda95791
Ikmalmughni <ikmalmughni@gmail.com>
2023-11-10 11:15:17 +0700

    gabungan main dan mqtt ubidots

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -1,37 +1,26 @@
-/****************************************
- * Include Libraries
- ****************************************/
-//#include "UbidotsESPMQTT.h"
 
-/****************************************
- * Define Constants
- ****************************************/
- /*
-#define TOKEN "BBUS-R7nOjqo6WS530rYHaAGmGsrjSr8duh" // Your Ubidots TOKEN
-#define WIFINAME "TP-Link_AFBC" //Your SSID
-#define WIFIPASS "Penelitian2023" // Your Wifi Pass
-*/
-/******************************************
- *
- * This example works for both Industrial and STEM users.
- *
- * Developed by Jose Garcia, https://github.com/jotathebest/
- *
- * ****************************************/
-
-/****************************************
- * Include Libraries
- ****************************************/
+#include <Arduino.h>
 #include "UbidotsEsp32Mqtt.h"
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-/****************************************
- * Define Constants
- ****************************************/
-const char *UBIDOTS_TOKEN = "BBUS-tt5el5LbI7OuFCwS92MYTJyzQD9zuq";  // Put here your Ubidots TOKEN
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//-------------------------------UBIDOTS---------------------------------------------------------------
+const char *UBIDOTS_TOKEN = "BBUS-R7nOjqo6WS530rYHaAGmGsrjSr8duh";  // Put here your Ubidots TOKEN
 const char *WIFI_SSID = "Kurniasyah";      // Put here your Wi-Fi SSID
 const char *WIFI_PASS = "Izzulizzam2002";      // Put here your Wi-Fi password
-const char *DEVICE_LABEL = "ESP32DevKit";   // Put here your Device label to which data  will be published
-const char *VARIABLE_LABEL = "Device-1"; // Put here your Variable label to which data  will be published
+
+//int adressDevice = 57; // Nilai integer
+//const char *topicPrefix = "01"; // String topik awal
+//char fullTopic[50]; // Array char untuk menyimpan topik MQTT lengkap
+
+
+const char *DEVICE_LABEL = "01";   // Put here your Device label to which data  will be published
+const char *VARIABLE_LABEL = "PushUpGraph"; // Put here your Variable label to which data  will be published
 
 const int PUBLISH_FREQUENCY = 500; // Update rate in milliseconds
 
@@ -40,9 +29,7 @@ uint8_t analogPin = 32; // Pin used to read data from GPIO34 ADC_CH6.
 
 Ubidots ubidots(UBIDOTS_TOKEN);
 
-/****************************************
- * Auxiliar Functions
- ****************************************/
+
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -56,14 +43,29 @@ void callback(char *topic, byte *payload, unsigned int length)
   Serial.println();
 }
 
-/****************************************
- * Main Functions
- ****************************************/
+//--------------------------------UBIDOTS-------------------------------------------------------
+
+//---------------------------------COUNTER------------------------------------------------------
+
+int pushUpSkor = 0, pushUpThresholdVL = 15;
+bool flag = false;
+
+VL53L0X sensor;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int i = 0;
+int x = 0;
+double rata;
+bool start = false;
+
+//---------------------------------COUNTER------------------------------------------------------
+
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
   // ubidots.setDebug(true);  // uncomment this to make debug messages available
   ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
   ubidots.setCallback(callback);
@@ -71,6 +73,27 @@ void setup()
   ubidots.reconnect();
 
   timer = millis();
+  //-------------------------------------------------------------------------------------
+  Wire.begin();
+  sensor.init();
+  sensor.setTimeout(500);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3C for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+
+  if (!sensor.init())
+  {
+    Serial.println("Failed to initialize VL53L0X sensor!");
+    while (1)
+      ;
+  }
+
+  sensor.startContinuous();
 }
 
 void loop()
@@ -82,10 +105,36 @@ void loop()
   }
   if ((millis() - timer) > PUBLISH_FREQUENCY) // triggers the routine every 5 seconds
   {
-    float value = analogRead(analogPin);
-    ubidots.add(VARIABLE_LABEL, value); // Insert your variable Labels and the value to be sent
+
+    uint16_t VLdistance = sensor.readRangeContinuousMillimeters()/10;
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("VL-mode");
+    display.setCursor(0, 30);
+    display.println("Count:");
+    display.setCursor(80, 30);
+    display.println(pushUpSkor);
+    display.display();
+    ubidots.add(VARIABLE_LABEL, VLdistance); // Insert your variable Labels and the value to be sent
     ubidots.publish(DEVICE_LABEL);
     timer = millis();
+
+    if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
+    {
+      pushUpSkor += 1;
+      Serial.print("Skor Push Up = ");
+      Serial.println(pushUpSkor);
+      flag = true;
+    }
+    if (VLdistance > pushUpThresholdVL)
+    {
+      flag = false;
+    }
+    //snprintf(fullTopic, sizeof(fullTopic), "%s/%d", topicPrefix, adressDevice);
+
   }
+  delay(100);
   ubidots.loop();
 }
