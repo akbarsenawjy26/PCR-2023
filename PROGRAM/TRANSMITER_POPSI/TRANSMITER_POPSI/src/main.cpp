b4b4f440a7bf78d5de4bfbdd907a7198b13db819
korosen123 <111441726+korosen123@users.noreply.github.com>
2023-11-21 16:41:35 +0700

    Update main.cpp

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -14,14 +14,101 @@ bool flag = false;
 VL53L0X sensor;
 Adafruit_SH1106 display(OLED_SDA, OLED_SCL);
 
+int Value1 = 9999, Value2 = 999;
+
+const int panjang_data = 1+1+1;
 int i = 0;
-int x = 0;
-double rata;
-bool start = false;
+byte nilai[panjang_data];
+
+int slave_index = 1; // PENTING!!! isi sinteger sesuai slave ke berapa yang akan di upload
+const int banyak_slave = 3;
+byte address_slave[banyak_slave] = {0x45,0x46,0x47};
+
+byte kode = 0x42;
+
+const int panjang_data_kirim = 4;
+byte nilai_kirim[panjang_data_kirim];
+byte checksum[2];
+int jumlah_data = 0;
+
+void checksum_serial() {
+  jumlah_data = 0;
+
+  if(nilai[0] == kode){
+    if (nilai[1] == address_slave[slave_index-1]) {
+      switch (nilai[2])
+      {
+      case 0x43:
+        // Master meminta data
+        // Proses mengirim data yang diminta
+        nilai_kirim[0] = (Value1 >> 8) & 0xFF;
+        nilai_kirim[1] = Value1 & 0xFF;
+        nilai_kirim[2] = (Value2 >> 8) & 0xFF;
+        nilai_kirim[3] = Value2 & 0xFF;
+
+        for(int i=0;i < panjang_data_kirim; i++){
+          jumlah_data += nilai_kirim[i];
+        }
+
+        checksum[0] = (jumlah_data >> 8) & 0xFF;
+        checksum[1] = jumlah_data & 0xFF;
+
+        Serial2.write(nilai_kirim,panjang_data_kirim);
+        Serial2.write(checksum,2);
+        break;
+
+      case 0x44:
+        // Master mengirim data
+        break;
+
+      default:
+        break;
+      }
+    } else {
+      Serial.println("Berbeda Address. Address ini : " + (String)address_slave[slave_index-1] + ". Sedangkan address yang dikirimkan" + (String)nilai[1]);
+    }
+  }else{
+    Serial.println("GAGAL!");
+  }
+
+  // empty the receiver buffer
+  while (Serial2.available())
+  {
+    Serial2.read();
+  }
+}
+
+void getSkor() {
+  uint16_t VLdistance = sensor.readRangeContinuousMillimeters();
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("VL-mode");
+  display.setCursor(80, 30);
+  display.println(VLdistance);
+  display.display();
+
+  if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
+  {
+    pushUpSkor += 1;
+    // Serial.print("Jarak = ");
+    // Serial.println(VLdistance);
+    flag = true;
+  }
+  if (VLdistance > pushUpThresholdVL)
+  {
+    flag = false;
+  }
+
+  Value1 = VLdistance;
+  Value2 = pushUpSkor;
+}
 
 void setup()
 {
   Serial.begin(115200);
+  Serial2.begin(9600);
   WiFi.mode(WIFI_STA);
 
   Wire.begin();
@@ -43,27 +130,18 @@ void setup()
 
 void loop()
 {
-  uint16_t VLdistance = sensor.readRangeContinuousMillimeters();
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("VL-mode");
-  display.setCursor(80, 30);
-  display.println(VLdistance);
-  display.display();
+  getSkor();
 
-  if (VLdistance <= pushUpThresholdVL && flag == false && VLdistance != 0)
-  {
-    pushUpSkor += 1;
-    Serial.print("Jarak = ");
-    Serial.println(VLdistance);
-    flag = true;
+  if (Serial2.available() > 0) {    
+    nilai[i] = Serial2.read();
+    i++;
+    if(i > panjang_data-1){
+      i = 0;
+      checksum_serial();
+    }
+    // for(int k = 0; k<panjang_data;k++){
+    //   Serial.print(nilai[k]);
+    // }
+    // Serial.println(); 
   }
-  if (VLdistance > pushUpThresholdVL)
-  {
-    flag = false;
-  }
-
-  delay(100);
 }
