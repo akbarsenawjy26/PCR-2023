6aab179e0d22d5e23bf56a7e913a730bc1f89136
meicans <meitiaracans@gmail.com>
2023-11-21 18:02:01 +0700

    Menu Baru

--- PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
+++ PROGRAM/RECIVER_POPSI/RECIVER_POPSI/src/main.cpp
@@ -1,18 +1,177 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define OLED_SDA 21
+#define OLED_SCL 22
+#define BLUE            0x001F
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int jumlah=21;
+
+Adafruit_SH1106 display(OLED_SDA, OLED_SCL);
+
+void start(){
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(20,20);
+  display.println("START");
+  display.display();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/*void menu(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(18,18);
+  display.println("Pribadi");
+  display.setCursor(18,30);
+  display.println("Bersama");
+  display.setCursor(18,42);
+  display.println("Set Jarak");
+  display.display();
+}*/
+
+void pribadi(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(0,18);
+  display.println("-> Pribadi");
+  display.setCursor(18,30);
+  display.println("Bersama");
+  display.setCursor(18,42);
+  display.println("Set Jarak");
+  display.display();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void bersama(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(18,18);
+  display.println("Pribadi");
+  display.setCursor(0,30);
+  display.println("-> Bersama");
+  display.setCursor(18,42);
+  display.println("Set Jarak");
+  display.display();
+}
+
+void setjarak(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(18,18);
+  display.println("Pribadi");
+  display.setCursor(18,30);
+  display.println("Bersama");
+  display.setCursor(0,42);
+  display.println("-> Set Jarak");
+  display.display();
+}
+
+/*void bersama2(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(40,0);
+  display.println("Bersama:");
+  display.setCursor(18,18);
+  display.println("Wired");
+  display.setCursor(18,30);
+  display.println("Wireless");
+  display.display();
+}*/
+
+void wired(){
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(40,0);
+  display.println("Bersama:");
+  display.setTextSize(1);
+  display.setCursor(0,18);
+  display.println("-> Wired");
+  display.setCursor(18,30);
+  display.println("Wireless");
+  display.display();
+}
+
+void wireless(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(40,0);
+  display.println("Bersama:");
+  display.setCursor(18,18);
+  display.println("Wired");
+  display.setCursor(0,30);
+  display.println("-> Wireless");
+  display.display();
+}
+
+void TotalPU(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(24,0);
+  display.println("Total Push Up:");
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(50,25);
+  display.println(jumlah);
+  display.display();
+}
+
+void press(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,28);
+  display.println("Press Button to Start");
+  display.display();
+}
+
+void countdown(){
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(56,22);
+  display.println("3");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(56,22);
+  display.println("2");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(56,22);
+  display.println("1");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+}
+
+void setup()   {                
+  Serial.begin(115200);
+  /* initialize OLED with I2C address 0x3C */
+  display.begin(SH1106_SWITCHCAPVCC, 0x3C); 
+  display.clearDisplay();
+
+}
+void loop() { 
+  //start();
+  //pribadi();
+  //bersama();
+  //setjarak();
+  wired();
+  //wireless();
+  //press();
+  //countdown();
+  //TotalPU();
 }
