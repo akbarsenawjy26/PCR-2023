673c45ef9c9d6a5f925614c3279a14d8af1c0d60
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2023-11-21 21:21:10 +0700

    Update main.cpp
    RS485 sudah masuk mode
    ESPNOW belum
    MQTT belum

--- PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
+++ PROGRAM/TRANSMITER_POPSI/TRANSMITER_POPSI/src/main.cpp
@@ -31,6 +31,159 @@ byte nilai_kirim[panjang_data_kirim];
 byte checksum[2];
 int jumlah_data = 0;
 
+int jumlah=21;
+
+int mode_tampilan = 0;
+int tombol = 0;
+bool tombol_up_ditekan = 0;
+bool tombol_down_ditekan = 0;
+bool tombol_set_ditekan = 0;
+
+void start(){
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(20,20);
+  display.println("START");
+  display.display();
+}
+
+/*void menu(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(18,18);
+  display.println("Pribadi");
+  display.setCursor(18,30);
+  display.println("Bersama");
+  display.setCursor(18,42);
+  display.println("Set Jarak");
+  display.display();
+}*/
+
+void pribadi(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(0,18);
+  display.println("-> Pribadi");
+  display.setCursor(18,30);
+  display.println("Bersama");
+  display.setCursor(18,42);
+  display.println("Set Jarak");
+  display.display();
+}
+
+void bersama(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(18,18);
+  display.println("Pribadi");
+  display.setCursor(0,30);
+  display.println("-> Bersama");
+  display.setCursor(18,42);
+  display.println("Set Jarak");
+  display.display();
+}
+
+void setjarak(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(48,0);
+  display.println("Menu");
+  display.setCursor(18,18);
+  display.println("Pribadi");
+  display.setCursor(18,30);
+  display.println("Bersama");
+  display.setCursor(0,42);
+  display.println("-> Set Jarak");
+  display.display();
+}
+
+/*void bersama2(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(40,0);
+  display.println("Bersama:");
+  display.setCursor(18,18);
+  display.println("Wired");
+  display.setCursor(18,30);
+  display.println("Wireless");
+  display.display();
+}*/
+
+void wired(){
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(40,0);
+  display.println("Bersama:");
+  display.setTextSize(1);
+  display.setCursor(0,18);
+  display.println("-> Wired");
+  display.setCursor(18,30);
+  display.println("Wireless");
+  display.display();
+}
+
+void wireless(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(40,0);
+  display.println("Bersama:");
+  display.setCursor(18,18);
+  display.println("Wired");
+  display.setCursor(0,30);
+  display.println("-> Wireless");
+  display.display();
+}
+
+void TotalPU(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(24,0);
+  display.println("Total Push Up:");
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(50,25);
+  display.println(jumlah);
+  display.display();
+}
+
+void press(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,28);
+  display.println("Press Button to Start");
+  display.display();
+}
+
+void countdown(){
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(56,22);
+  display.println("3");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(56,22);
+  display.println("2");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(56,22);
+  display.println("1");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+}
+
 void checksum_serial() {
   jumlah_data = 0;
 
@@ -137,21 +290,113 @@ void setup()
 
 void loop()
 {
-  if (i==0) 
-  {
-    getSkor();
+  if(mode_tampilan == 0){
+    start();
+    mode_tampilan = 1;
+  }else if(mode_tampilan == 1){
+    if(tombol_down_ditekan == HIGH){
+      tombol++;
+      if(tombol>2){
+        tombol = 0;
+      }
+    }
+    
+    if(tombol == 0){
+      pribadi();
+    }else if(tombol == 1){
+      bersama();
+    }else if(tombol == 2){
+      setjarak();
+    }
+
+    if((tombol == 0) && (tombol_set_ditekan == HIGH)){
+      mode_tampilan = 2;
+    }else if((tombol == 1) && (tombol_set_ditekan == HIGH)){
+      mode_tampilan = 3;
+    }else if((tombol == 2) && (tombol_set_ditekan == HIGH)){
+      mode_tampilan = 4;
+    }
+  }else if(mode_tampilan == 2){
+    if(tombol_set_ditekan == HIGH){
+      countdown();
+      mode_tampilan == 5; //menu mqtt
+    }else{
+      press();
+    }
+  }else if(mode_tampilan == 3){
+    if(tombol_down_ditekan == HIGH){
+      tombol++;
+      if(tombol>1){
+        tombol = 0;
+      }
+    }
+    
+    if(tombol == 0){
+      wired();
+    }else if(tombol == 1){
+      wireless();
+    }
+
+    if((tombol == 0) && (tombol_set_ditekan == HIGH)){
+      mode_tampilan = 6; //menu rs485
+    }else if((tombol == 1) && (tombol_set_ditekan == HIGH)){
+      mode_tampilan = 7; //menu espnow
+    }
+  }else if(mode_tampilan == 4){
+    if(tombol_set_ditekan == HIGH){
+      countdown();
+      mode_tampilan = 8;//menu setjarak
+    }else{
+      press();
+    }
+  }else if(mode_tampilan == 5){
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(40,0);
+    display.println("MQTT:");
+    if(tombol_set_ditekan == HIGH){
+      mode_tampilan = 9;
+    }
+  }else if(mode_tampilan == 6){
+    if (i==0) 
+    {
+      getSkor();
+    }
+
+    if (Serial2.available() > 0) {    
+      nilai[i] = Serial2.read();
+      i++;
+      if(i > panjang_data-1){
+        i = 0;
+        checksum_serial();
+      }
+      // for(int k = 0; k<panjang_data;k++){
+      //   Serial.print(nilai[k]);
+      // }
+      // Serial.println(); 
+    }
+    if(tombol_set_ditekan == HIGH){
+      mode_tampilan = 9;
+    }
+    
+  }else if(mode_tampilan == 7){
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(40,0);
+    display.println("ESPNOW:");
+    if(tombol_set_ditekan == HIGH){
+      mode_tampilan = 9;
+    }
+  }else if(mode_tampilan == 8){
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(40,0);
+    display.println("SETJARAK:");
+    if(tombol_set_ditekan == HIGH){
+      mode_tampilan = 9;
+    }
+  }else if(mode_tampilan == 9){
+    TotalPU();
   }
 
-  if (Serial2.available() > 0) {    
-    nilai[i] = Serial2.read();
-    i++;
-    if(i > panjang_data-1){
-      i = 0;
-      checksum_serial();
-    }
-    // for(int k = 0; k<panjang_data;k++){
-    //   Serial.print(nilai[k]);
-    // }
-    // Serial.println(); 
-  }
 }
